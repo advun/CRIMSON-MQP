45616bbc57e2ea30596cd6692be1dd83652116a4
advun <cameronpwilding@gmail.com>
2025-09-29 05:22:21 -0400

    Create next_state.v

---
+++ next_state.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/29/2025 04:52:32 AM
+// Design Name: 
+// Module Name: next_state
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module next_state(
+    input wire [6:0] blocks,
+    input wire [2:0] selector,
+    input wire [3:0] current_state,
+    output wire [3:0] next_state
+    );
+    
+endmodule
