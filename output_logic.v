abc618ee6ca5f0cb9e54b0e874b993dfc0e5162f
advun <cameronpwilding@gmail.com>
2025-09-29 05:23:11 -0400

    Create output_logic.v

---
+++ output_logic.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/29/2025 04:52:32 AM
+// Design Name: 
+// Module Name: next_state
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module output_logic(
+    input wire [3:0] current_state,
+    input wire [2:0] selector,
+    output wire [6:0] control
+    );
+endmodule
