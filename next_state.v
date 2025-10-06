e695f7c09c8682be648c868d68d73c69ced5ecff
advun <cameronpwilding@gmail.com>
2025-10-06 06:37:22 -0400

    Update next_state.v

--- next_state.v
+++ next_state.v
@@ -21,10 +21,20 @@
 
 
 module next_state(
-    input wire [6:0] blocks,
-    input wire [2:0] selector,
-    input wire [3:0] current_state,
-    output wire [3:0] next_state
+    input wire solar_on, //solar voltage is high enough
+    input wire always_on, //if high, can't go to sleep mode
+    input wire low_power, //if high, low power operation
+    input wire reset_n, //active low reset
+    input wire [2:0] current_state,
+    output wire [2:0] next_state
     );
+    //state 00: Reset (reset_n low in any state)
+    //state 01: Normal Operation (leave Reset when low_power = 0 and solar_on = 1)
+    //state 10: Low Power Operation (leave Reset when low_power = 1 and solar_on = 1)
+    //state 11: Sleep Mode (solar_on is low in any state but reset)
+    
+   assign next_state[0] = (solar_on & ~low_power & reset_n) | (~solar_on & reset_n & ~always_on) ;
+   assign next_state[1] = (solar_on & low_power & reset_n) | (~solar_on & reset_n & ~always_on);
+   //assign next_state[2] = ;
     
 endmodule
