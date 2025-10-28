e2f9a7ddd5a948e194a403516fe863af9667e4f1
advun <cameronpwilding@gmail.com>
2025-10-27 21:04:10 -0400

    Update and rename ADC/ADCregsSIM.v to ADC/Sim/ADCregsSIM.v

--- ADC/Sim/ADCregsSIM.v
+++ ADC/Sim/ADCregsSIM.v
@@ -51,11 +51,18 @@ module ADCregsSIM(
     #20;
     
     reset_n = 1;
+    enable = 1;
     
     #10;
     
     in = 1;
-    #100;
+    #10;
+    in = 1;
+    #10;
+    in = 0;
+    #10;
+    in = 1;
+    #10;
     
     $finish;
     
